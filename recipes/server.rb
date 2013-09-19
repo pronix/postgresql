95fca009134fceedd6bc34d0ed15463f527773e8
dima <pronix.service@gmail.com>
2013-09-19 12:46:24 +0200

    typo fix

--- recipes/server.rb
+++ recipes/server.rb
@@ -96,7 +96,7 @@ bash "create replica user" do
 echo "CREATE USER #{node['postgresql']['recovery_user']} REPLICATION ENCRYPTED PASSWORD '#{node['postgresql']['recovery_user_pass']}';" | psql
   EOH
   action :run
-  only_if { node['postgres']['recovery_user'].size > 0 && node['postgres']['recovery_user_pass'].size > 0 }
+  only_if { node['postgresql']['recovery_user'].size > 0 && node['postgresql']['recovery_user_pass'].size > 0 }
 end
 
 bash "assign-postgres-password" do
